e3e74eb1574f45d5c12bb719766ac907df053111
ronaldofreitas41 <ronaldinho.lfs@gmail.com>
2026-05-07 16:23:38 -0300

    feat: update package.json and main.ts to integrate Swagger and dotenv configurations

--- services/wallets/src/main.ts
+++ services/wallets/src/main.ts
@@ -1,12 +1,26 @@
-import "reflect-metadata";
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { config } from 'dotenv'
 
-async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT;
-  await app.listen(port, "0.0.0.0");
-  console.log(`Wallets service running on port ${port}`);
+config()
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Crash Game API')
+    .setDescription('API do crash game')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('docs', app, document)
+  const port = process.env.PORT || 4001
+  
+  await app.listen(port, () => {
+    console.log(`Wallets service is running on port ${port}`)
+  })
 }
-
-bootstrap();
